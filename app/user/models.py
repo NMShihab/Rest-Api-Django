e779ceb5b4724ca93e82e467b7399cbc3a7969f8
NMShihab <nmshihabislam@gmail.com>
2021-03-08 19:29:02 +0600

    fix some flake8

--- app/user/models.py
+++ app/user/models.py
@@ -1,27 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import  AbstractBaseUser,\
+from django.contrib.auth.models import AbstractBaseUser,\
     BaseUserManager, PermissionsMixin
 
 
 # Customize UserManager Class
-class  UserManager(BaseUserManager):
-    def create_user(self, email,password = None, **extra_fields):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
         """Create and Save a New User"""
         if not email:
             raise ValueError("You must inpul valid email")
-        user = self.model(email = self.normalize_email(email), **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         # Set encrypted password
         user.set_password(password)
-        user.save(using = self._db) # (using = self._db ) it will help us to use multiple database
-
+        # (using = self._db ) multiple database  
+        user.save(using=self._db)        
         return user 
 
-    def create_superuser(self,email, password):
+    def create_superuser(self, email, password):
         """Create super User and save new one"""
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -29,13 +29,10 @@ class  UserManager(BaseUserManager):
 # Customize User model class
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom User Models That Will Help Us using Email as username"""
-
-    email = models.EmailField(max_length = 300, unique = True)
-    name = models.CharField(max_length = 300)
-    is_active = models.BooleanField(default= True)
-    is_staff = models.BooleanField(default = True)
-
+    email = models.EmailField(max_length=300, unique=True)
+    name = models.CharField(max_length=300)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
     objects = UserManager()
-
     # Make email as username field
     USERNAME_FIELD = 'email'
--- app/user/tests/test_models.py
+++ app/user/tests/test_models.py
@@ -1,19 +1,17 @@
 from django.test import TestCase
-from django.contrib.auth import  get_user_model
+from django.contrib.auth import get_user_model
 
 
 class ModelTes(TestCase):
     def test_user_created(self):
-        """Test using a new user with email"""
-        
+        """Test using a new user with email"""       
         email = "test@gmail.com"
         password = "test123"
 
         # Creating user
-        user = get_user_model().objects.create_user(
-            
-            email = email,
-            password = password
+        user = get_user_model().objects.create_user(         
+            email=email,
+            password=password
         )
 
         # Test email and password
@@ -23,12 +21,11 @@ class ModelTes(TestCase):
     def test_new_user_email_normalized(self):
         """Test the new user email is normalized"""
         email = "test@GMAIL.COM"
-        user = get_user_model().objects.create_user(email,"test@123")
+        user = get_user_model().objects.create_user(email, "test@123")
 
-        self.assertEqual(user.email,email.lower())
-    
-    def test_new_user_invalid_email(self):
-        
+        self.assertEqual(user.email, email.lower())
+ 
+    def test_new_user_invalid_email(self):       
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
